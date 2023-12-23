4b5de11d3d8a6f940207cec18a34ca717994cc0d
ardattoprak <ardattoprak@gmail.com>
2023-12-23 16:53:05 +0300

    Initial commit

---
+++ app/src/main/java/msku/ceng/madlab/tarimlaapp/RegisterPageActivity.java
@@ -0,0 +1,14 @@
+package msku.ceng.madlab.tarimlaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+    }
+}
