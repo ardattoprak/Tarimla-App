c2c7d75be0967fcb7cf0eafd42a974ff279fe9dc
ardattoprak <ardattoprak@gmail.com>
2023-12-30 00:23:14 +0300

    FirebaseAuth embeded and RegisterPageActivity filled.

--- app/src/main/java/msku/ceng/madlab/tarimlaapp/RegisterPageActivity.java
+++ app/src/main/java/msku/ceng/madlab/tarimlaapp/RegisterPageActivity.java
@@ -1,14 +1,103 @@
 package msku.ceng.madlab.tarimlaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterPageActivity extends AppCompatActivity {
+
+    EditText nameText,lastNameText, emailText, passwordText, wareHouseText, phoneNumberText;
+    TextView goToLoginPageText;
+    Button registerButton;
+    ProgressBar progressBar1;
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        nameText = findViewById(R.id.name_signup_edit_text);
+        lastNameText = findViewById(R.id.last_name_signup_edit_text);
+        emailText = findViewById(R.id.email_signup_edit_text);
+        passwordText = findViewById(R.id.password_signup_edit_text);
+        wareHouseText = findViewById(R.id.warehouse_capacity_signup_edit_text);
+        phoneNumberText = findViewById(R.id.phone_number_signup_edit_text);
+        registerButton = findViewById(R.id.register_signup_button);
+        progressBar1 = findViewById(R.id.progressBar1);
+        goToLoginPageText = findViewById(R.id.gotToLogin);
+
+        goToLoginPageText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterPageActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar1.setVisibility(View.VISIBLE);
+                String email, password;
+
+                email = String.valueOf(emailText.getText());
+                password = String.valueOf(passwordText.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(RegisterPageActivity.this,"Enter email",Toast.LENGTH_LONG);
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterPageActivity.this,"Enter Password",Toast.LENGTH_LONG);
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar1.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    // BURAYI BEN KAPADIM FirebaseUser user = mAuth.getCurrentUser();
+
+                                    Toast.makeText(RegisterPageActivity.this, "Account Created.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(RegisterPageActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+
     }
 }
